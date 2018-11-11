2a813a0368500234c85a754523e2eb7e5e49ff3c
Septian Riza Saputra <septian.bhoechie@gmail.com>
2018-11-12 01:51:20 +0700

    update

--- api-product/routes/product.route.js
+++ api-product/routes/product.route.js
@@ -4,8 +4,9 @@ const router = express.Router();
 // Require the controllers
 const product_controller = require('../controllers/product.controller');
 
+router.post('/list', product_controller.list);
 router.post('/create', product_controller.create);
 router.get('/show/:id', product_controller.show);
-router.put('/edit/:id', product_controller.edit);
+router.put('/edit/:id', product_controller.update);
 router.delete('/delete/:id', product_controller.delete);
 module.exports = router;

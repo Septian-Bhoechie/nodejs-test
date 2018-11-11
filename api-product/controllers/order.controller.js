e8c21c130105f31dd0cdf79d8a7c30b9f461aa95
Septian Riza Saputra <septian.bhoechie@gmail.com>
2018-11-12 01:50:10 +0700

    update

--- api-product/controllers/order.controller.js
+++ api-product/controllers/order.controller.js
@@ -4,66 +4,66 @@ const OrderDetail = require('../models/order_detail.model');
 
 exports.submit = function (req, res) {
 
-		let order = new Order(
-				{
-						grand_total: 0,
-						order_number: Math.floor(Math.random() * 11);
-				}
-		);
+    let order = new Order(
+        {
+            grand_total: 0,
+            order_number: Math.floor(Math.random() * 11);
+        }
+    );
 
-		order.save(function (err) {
-				if (err) {
-						return next(err);
-				}
+    order.save(function (err) {
+        if (err) {
+            return next(err);
+        }
 
-				var grandTotal = 0;
+        var grandTotal = 0;
 
-				Product.find({
-					'_id': { $in: req.body.ids }
-				}).exec(function(err2, products) {
-						if (err2) {
-								return next(err2);
-						}
-						else {
-							products.forEach( function(product, index) {
-								if (product && product._id) {
-										let orderDetail = new OrderDetail(
-												{
-														order: order._id,
-														product: product._id,
-														price: product.price,
-														qty: req.body.qtys[product._id];
-														total: parseInt(req.body.qtys[product._id]) * product.price;
-												}
-										);
-										orderDetail.save(function (err3) {
-											if (!err3) {
-													grandTotal += orderDetail.total;
-											}
-										});
-								}
-							});
-						}
+        Product.find({
+          '_id': { $in: req.body.ids }
+        }).exec(function(err2, products) {
+            if (err2) {
+                return next(err2);
+            }
+            else {
+              products.forEach( function(product, index) {
+                if (product && product._id) {
+                    let orderDetail = new OrderDetail(
+                        {
+                            order: order._id,
+                            product: product._id,
+                            price: product.price,
+                            qty: req.body.qtys[product._id];
+                            total: parseInt(req.body.qtys[product._id]) * product.price;
+                        }
+                    );
+                    orderDetail.save(function (err3) {
+                      if (!err3) {
+                          grandTotal += orderDetail.total;
+                      }
+                    });
+                }
+              });
+            }
 
-						//if all products was insert to order details
-						//update order grand total
-						order.grand_total = grandTotal;
-						order.save(function(err3) {
-							if (err3) {
-								return next(err3);
-							}
+            //if all products was insert to order details
+            //update order grand total
+            order.grand_total = grandTotal;
+            order.save(function(err3) {
+              if (err3) {
+                return next(err3);
+              }
 
-							Order
-								.findById(order._id)
-								.populate('details')
-								.exec(function (err4, orderResult) {
-									if (err4) {
-										return next(err3);
-									}
+              Order
+                .findById(order._id)
+                .populate('details')
+                .exec(function (err4, orderResult) {
+                  if (err4) {
+                    return next(err3);
+                  }
 
-									res.send(orderResult);
-								});
-						});
-				});
-		})
+                  res.send(orderResult);
+                });
+            });
+        });
+    })
 };
